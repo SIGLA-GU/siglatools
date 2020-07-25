c70e9b8e9eb9d60bd217425da0a36127be4c54ef
To Huynh <huynh.nto@gmail.com>
2020-07-24 21:37:48 -0700

    Delete base classes, remove pipelines modules

--- siglatools/pipelines/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .institution_etl_pipeline import InstitutionETLPipeline  # noqa: F401
-from .pipeline import Pipeline  # noqa: F401
--- siglatools/pipelines/institution_etl_pipeline.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .pipeline import Pipeline
-
-
-class InstitutionETLPipeline(Pipeline):
-    pass
