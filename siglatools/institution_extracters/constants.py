a35be85116b8bb94ce3aab2dfba085191c7f403c
To Huynh <huynh.nto@gmail.com>
2020-07-28 15:45:40 -0700

    Add institution-and-composite-variable and composite-variable gs formats

--- siglatools/institution_extracters/constants.py
+++ siglatools/institution_extracters/constants.py
@@ -5,3 +5,5 @@
 class GoogleSheetsFormat:
     institution_by_triples = "institution-by-triples"
     institution_by_rows = "institution-by-rows"
+    institution_and_composite_variable = "institution-and-composite-variable"
+    composite_variable = "composite-variable"
