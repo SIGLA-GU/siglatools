0400ec5c363138cd1ba0d5fd1e0a384355ab05aa
To Huynh <huynh.nto@gmail.com>
2020-07-28 15:41:41 -0700

    Add more database collection names and variable type constants

--- siglatools/databases/constants.py
+++ siglatools/databases/constants.py
@@ -5,3 +5,11 @@
 class DatabaseCollection:
     institutions = "institutions"
     variables = "variables"
+    rights = "rights"
+    amendments = "amendments"
+    legal_framework = "legal_framework"
+
+
+class VariableType:
+    standard = "standard"
+    composite = "composite"
