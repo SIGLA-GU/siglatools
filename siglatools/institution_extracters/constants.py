6129d88a29705ebd74fb1716c880acdf88dadd91
To Huynh <huynh.nto@gmail.com>
2020-08-10 16:00:05 -0700

    Update gs format

--- siglatools/institution_extracters/constants.py
+++ siglatools/institution_extracters/constants.py
@@ -3,7 +3,8 @@
 
 
 class GoogleSheetsFormat:
-    institution_by_triples = "institution-by-triples"
+    standard_institution = "standard-institution"
     institution_by_rows = "institution-by-rows"
     institution_and_composite_variable = "institution-and-composite-variable"
     composite_variable = "composite-variable"
+    multiple_sigla_answer_variable = "multiple-sigla-answer-variable"
