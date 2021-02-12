3426b30fc3e9564a37718be74bd9f4889c17f0f2
To Huynh <37560480+tohuynh@users.noreply.github.com>
2021-02-11 20:58:24 -0800

    feature/link-checker (#11)
    * Add workflows
    
    * Add skeleton link checker file

---
+++ siglatools/bin/run_external_link_checker.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script will get deployed in the bin directory of the
+users' virtualenv when the parent module is installed using pip.
+"""
+
+import argparse
+import logging
+import sys
+import traceback
+
+from siglatools import get_module_version
+
+###############################################################################
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(levelname)4s:%(lineno)4s %(asctime)s] %(message)s"
+)
+log = logging.getLogger()
+
+###############################################################################
+
+
+def run_external_link_checker(
+    master_spreadsheet_id: str,
+    google_api_credentials_path: str,
+):
+    print(
+        f"Running link checker for {master_spreadsheet_id} {google_api_credentials_path}"
+    )
+
+
+###############################################################################
+# Args
+
+
+class Args(argparse.Namespace):
+    def __init__(self):
+        self.__parse()
+
+    def __parse(self):
+        # Set up parser
+        p = argparse.ArgumentParser(
+            prog="run_external_link_checker",
+            description="A script to run external link checker",
+        )
+        # Arguments
+        p.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version="%(prog)s " + get_module_version(),
+        )
+        p.add_argument(
+            "-msi",
+            "--master_spreadsheet_id",
+            action="store",
+            dest="master_spreadsheet_id",
+            type=str,
+            help="The master spreadsheet id",
+        )
+        p.add_argument(
+            "-gacp",
+            "--google_api_credentials_path",
+            action="store",
+            dest="google_api_credentials_path",
+            type=str,
+            help="The google api credentials path",
+        )
+        p.add_argument(
+            "--debug", action="store_true", dest="debug", help=argparse.SUPPRESS
+        )
+        # Parse
+        p.parse_args(namespace=self)
+
+
+###############################################################################
+
+
+def main():
+    try:
+        args = Args()
+        dbg = args.debug
+        run_external_link_checker(
+            args.master_spreadsheet_id,
+            args.google_api_credentials_path,
+        )
+    except Exception as e:
+        log.error("=============================================")
+        if dbg:
+            log.error("\n\n" + traceback.format_exc())
+            log.error("=============================================")
+        log.error("\n\n" + str(e) + "\n")
+        log.error("=============================================")
+        sys.exit(1)
+
+
+###############################################################################
+# Allow caller to directly run this module (usually in development scenarios)
+
+if __name__ == "__main__":
+    main()
