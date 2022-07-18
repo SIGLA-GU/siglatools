af3b3cb41e4e8169807f238d0973d1dd910b1dd5
To Huynh <huynh.nto@gmail.com>
2022-07-18 12:39:57 -0700

    Change author

--- siglatools/__init__.py
+++ siglatools/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for SIGLA Tools."""
 
-__author__ = "To Huynh"
+__author__ = "SIGLA"
 __email__ = "huynh.nto@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
