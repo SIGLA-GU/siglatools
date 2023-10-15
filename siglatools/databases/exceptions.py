5f9be4317b4504bd713ffdd2e6ec1a23d12ac3d9
To Huynh <huynh.nto@gmail.com>
2023-10-14 19:45:10 -0700

    Add .

--- siglatools/databases/exceptions.py
+++ siglatools/databases/exceptions.py
@@ -6,4 +6,4 @@ from ..utils.exceptions import BaseError, ErrorInfo
 
 class UnableToFindDocument(BaseError):
     def __init__(self, info: ErrorInfo):
-        super().__init__("Unable to find document in database", info)
+        super().__init__("Unable to find document in database.", info)
